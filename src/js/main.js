d666c33706220b11c6fa3f5e1b9cac34a8549f88
I Black <itomblack@hotmail.co.uk>
2017-11-06 21:58:11 -0800

    continue CV

--- src/js/main.js
+++ src/js/main.js
@@ -50,6 +50,86 @@
 
 
 
+//********** CV *************** //
+var cvjob1 = new Vue({
+  el: '#cv-job-1',
+  data: {
+    company: 'Invoke',
+    dates: 'May 2017 - Present',
+    role: 'Senior Product Designer',
+    bullets: [
+      { message: 'Product design for established businesses, & fresh startups.' },
+      { message: 'Organizing & facilitating design workshops for clients & minor celebrities.' },
+      { message: 'Collaborating with strategists & developers.' },
+      { message: 'Developing the Invoke brand tone of voice.' },
+      { message: 'Revamping design team recruitment processes.' }
+
+    ]
+  }
+})
+
+var cvjob2 = new Vue({
+  el: '#cv-job-2',
+  data: {
+    company: 'Beyond',
+    dates: 'Aug 2016 - Jan 2017',
+    role: 'Senior UX Designer',
+    bullets: [
+      { message: 'Defining new business products with the Beyond Strategy team.' },
+      { message: 'Developing agile, UX design processes within TravelSupermarket.' },
+      { message: 'Leading UX workshops for the UK\'s 2nd largest Supermarket.' },
+      { message: 'Mentoring Junior Designers.' }
+    ]
+  }
+})
+
+
+var cvjob3 = new Vue({
+  el: '#cv-job-3',
+  data: {
+    company: 'Pebble {code}',
+    dates: 'Jan 2015 - Jul 2016',
+    role: 'UX/UI Designer',
+    bullets: [
+      { message: 'Performing research, product design & development of enterprise software for the UK\'s largest pharmaceutical company, AstraZeneca.' },
+      { message: 'Project & client management, from proposal to production.' },
+      { message: 'Organising hack-days to encourage experimentation & rapid prototyping, internally & with clients' },
+      { message: 'Contributing to culture by running art classes, blogging & presentations.' }
+    ]
+  }
+})
+
+var cvjob4 = new Vue({
+  el: '#cv-job-4',
+  data: {
+    company: 'Harrison:Fraser',
+    dates: 'Jan 2015 - Jul 2016',
+    role: 'Designer',
+    bullets: [
+      { message: 'Creating international brands for this strategic design consultancy.' },
+      { message: 'Designing across print, digital & interiors for the retail & leisure industries.' },
+      { message: 'Collaborating with brand planners, clients and suppliers to deliver engaging brand experiences.' }
+    ]
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ****** Vanilla JS Stuff ****** //
   
 
